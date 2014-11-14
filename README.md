ba837cad3a6c769d1d3862da1ebda0b7f5e7e578
Shoji Kai <sho2kai@gmail.com>
2014-11-14 15:28:43 +0900

    first commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# NAME
+
+Google::BigQuery - It's new $module
+
+# SYNOPSIS
+
+    use Google::BigQuery;
+
+# DESCRIPTION
+
+Google::BigQuery is ...
+
+# LICENSE
+
+Copyright (C) Shoji Kai.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+# AUTHOR
+
+Shoji Kai <sho2kai@gmail.com>
---
+++ lib/Google/BigQuery.pm
@@ -0,0 +1,39 @@
+package Google::BigQuery;
+use 5.008001;
+use strict;
+use warnings;
+
+our $VERSION = "0.01";
+
+
+
+1;
+__END__
+
+=encoding utf-8
+
+=head1 NAME
+
+Google::BigQuery - It's new $module
+
+=head1 SYNOPSIS
+
+    use Google::BigQuery;
+
+=head1 DESCRIPTION
+
+Google::BigQuery is ...
+
+=head1 LICENSE
+
+Copyright (C) Shoji Kai.
+
+This library is free software; you can redistribute it and/or modify
+it under the same terms as Perl itself.
+
+=head1 AUTHOR
+
+Shoji Kai E<lt>sho2kai@gmail.comE<gt>
+
+=cut
+
